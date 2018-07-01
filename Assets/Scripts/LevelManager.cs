9afdff5f61ea435a8a51c207615702b8a3b53f51
Mark Carroll <markjcarroll@hotmail.co.uk>
2018-07-01 20:47:33 +0100

    Initial commit Menu, Game Screens and Sprites Imported

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit Button Pressed");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentSceenIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceenIndex + 1);
+    }
+
+}
