605cc6b1f1975e90b8f0cd03e6d89dc6b9b434ab
Mark Carroll <markjcarroll@hotmail.co.uk>
2018-07-01 21:58:24 +0100

    Player controller added with movement limits set dynamicaly, started imlementing enemy spawning

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 15.0f;
+    float xMin = 0.0f;
+    float xMax = 0.0f;
+    public float padding = 0.5f;
+	// Use this for initialization
+	void Start ()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, distance));
+        xMin = leftmost.x + padding;
+        xMax = rightmost.x - padding;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position +=  Vector3.left * speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else
+        {
+
+        }
+        // restricting playyer to gamespace
+        float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
